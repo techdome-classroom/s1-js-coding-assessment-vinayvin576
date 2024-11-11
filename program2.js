e287dfb77e00c405f32ee758e77f6f071dc4eba0
vinayvin576 <130123822+vinayvin576@users.noreply.github.com>
2024-11-11 13:40:39 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,18 @@
 const decodeTheRing = function (s, p) {
+    function matchHelper(i, j) {
+        if (j === p.length) {
+            return i === s.length;
+        }
+        if (p[j] === '*') {
+            return (i < s.length && matchHelper(i + 1, j)) || matchHelper(i, j + 1);
+        }
+        if (i < s.length && (p[j] === '?' || p[j] === s[i])) {
+            return matchHelper(i + 1, j + 1);
+        }
+        return false;
+    }
+return matchHelper(0, 0);
+};
 
-    // write your code here
-
-  };
   
   module.exports = decodeTheRing;
