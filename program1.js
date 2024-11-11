daef9296661f9aa7951ac146c9174e02d42d7dae
vinayvin576 <130123822+vinayvin576@users.noreply.github.com>
2024-11-11 13:48:49 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,8 +1,28 @@
 const getTotalIsles = function (grid) {
+    if (!grid || grid.length === 0) return 0;
 
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let islandCount = 0;
+    function dfs(r, c) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'W') return;
 
-  // write your code here
+        grid[r][c] = 'W';
+        dfs(r + 1, c);
+        dfs(r - 1, c);
+        dfs(r, c + 1);
+        dfs(r, c - 1);
+    }
 
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === 'L') {
+                dfs(r, c);
+                islandCount++;
+            }
+        }
+    }
+
+    return islandCount;
 };
-
 module.exports = getTotalIsles;
